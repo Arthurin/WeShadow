c2f8f9955444e3657effa591f3c97bef8d1a9357
Flávio J. Saraiva <flaviojs2005@gmail.com>
2014-12-02 22:00:08 +0000

    Move MainWindow to the start.
    Visual Studio needs it to be first to add events automatically.

--- WorkingWithDepthData/MainWindow.xaml.cs
+++ WorkingWithDepthData/MainWindow.xaml.cs
@@ -22,6 +22,274 @@ using WpfAnimatedGif;
 
 namespace WorkingWithDepthData
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime now;
+        int birdPositionX = 500;
+        int birdPositionY = 50;
+
+        SparkleManager sparkleManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Canvas.SetLeft(this.birdStatic, 500);
+            Canvas.SetBottom(this.birdStatic, 50);
+
+            this.birdFly.Visibility = Visibility.Hidden;
+
+            System.Diagnostics.Debug.WriteLine("coucou");
+
+            sparkleManager = new SparkleManager(canvas);
+
+            //ImageBehavior.SetAnimatedSource(birdImage, image);
+        }
+        const float MaxDepthDistance = 4095; // max value returned
+        const float MinDepthDistance = 850; // min value returned
+        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            var oldSensor = (KinectSensor)e.OldValue;
+
+            //stop the old sensor
+            StopKinect(oldSensor);
+
+            //get the new sensor
+            var newSensor = (KinectSensor)e.NewValue;
+            try
+            {
+                StartKinect(newSensor);
+            }
+            catch (System.IO.IOException)
+            {
+                //this happens if another app is using the Kinect
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+        void newSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            now = DateTime.Now;
+            sparkleManager.Update(now);
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame == null)
+                {
+                    return; 
+                }
+
+                byte[] pixels = GenerateColoredBytes(depthFrame);
+
+                //number of bytes per row width * 4 (B,G,R,Empty)
+                int stride = depthFrame.Width * 4;
+
+                //create image
+                image1.Source = 
+                    BitmapSource.Create(depthFrame.Width, depthFrame.Height, 
+                    96, 96, PixelFormats.Bgr32, null, pixels, stride);
+
+                sparkleManager.Generate(now, depthFrame, pixels);
+            }
+        }
+
+        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
+        {
+
+            //get the raw data from kinect with the depth for every pixel
+            short[] rawDepthData = new short[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(rawDepthData); 
+
+            //use depthFrame to create the image to display on-screen
+            //depthFrame contains color information for all pixels in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
+
+            //Bgr32  - Blue, Green, Red, empty byte
+            //Bgra32 - Blue, Green, Red, transparency 
+            //You must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+            int x = 0;
+            int y = 0;
+
+            
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0; 
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length; 
+                depthIndex++, colorIndex += 4)
+            {
+                x++;
+                if (x == depthFrame.Width)
+                {
+                    y++;
+                    x = 0;
+                }
+                //get the player (requires skeleton tracking enabled for values)
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+
+                //gets the depth value
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                if (depth <= 0)
+                {
+                    //we are very close
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 255;
+
+                }
+                else if (depth > 0 && depth < 2500)
+                {
+                    //we are a bit further away
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+
+
+                    if (this.birdStatic.Visibility == Visibility.Visible && isNearPoint(x, y, birdPositionX, birdPositionY))
+                    {
+                        this.birdFly.Visibility = Visibility.Visible;
+                        this.birdStatic.Visibility = Visibility.Hidden;
+                        System.Diagnostics.Debug.Write(". On change tout ");
+                    }
+                }
+                else
+                {
+                    //we are the farthest
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 255;
+                }
+
+
+                //////equal coloring for monochromatic histogram
+                //byte intensity = CalculateIntensityFromDepth(depth);
+                //pixels[colorIndex + BlueIndex] = intensity;
+                //pixels[colorIndex + GreenIndex] = intensity;
+                //pixels[colorIndex + RedIndex] = intensity;
+
+
+                ////Color all players "gold"
+                //if (player > 0)
+                //{
+                //    pixels[colorIndex + BlueIndex] = Colors.Black.B;
+                //    pixels[colorIndex + GreenIndex] = Colors.Black.G;
+                //    pixels[colorIndex + RedIndex] = Colors.Black.R;
+                //}
+
+                /*
+                if ((x >= 100 && x <= 200) && (y >= 100 && y <= 150))
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 255;
+                }*/
+
+
+            }
+          
+
+            return pixels;
+        }
+
+        private bool isNearPoint(int x, int y, int pointPositionX, int pointPositionY)
+        {
+            //System.Diagnostics.Debug.WriteLine("Point detection: ");
+            //System.Diagnostics.Debug.WriteLine(p);
+            return (pointPositionX - 80 <= x && x <= pointPositionX + 80) && (pointPositionY - 80 <= y && y <= pointPositionY + 80);
+        }
+
+
+        public static byte CalculateIntensityFromDepth(int distance)
+        {
+            //formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0) 
+                / (MaxDepthDistanceOffset)));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopKinect(kinectSensorChooser1.Kinect); 
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+
+
+                }
+            }
+        }
+
+        private void StartKinect(KinectSensor sensor)
+        {
+            if (sensor == null)
+                return;
+
+            //turn on features that you need
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            sensor.SkeletonStream.Enable();
+
+            //sign up for events if you want to get at API directly
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(newSensor_AllFramesReady);
+
+            sensor.Start();
+        }
+
+        bool creatingFakeSpakles = false;
+        Point mousePosition;
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                creatingFakeSpakles = true;
+                mousePosition = e.GetPosition(this);
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                creatingFakeSpakles = false;
+                if (e.RightButton == MouseButtonState.Released)
+                {
+                    // create fake sparkle
+                    sparkleManager.AddSparkleFromMouse(mousePosition, e.GetPosition(this), now);
+                }
+            }
+        }
+
+    }
+
     class MovingSparkle
     {
         public System.Windows.Controls.Image Image;
@@ -296,274 +564,6 @@ namespace WorkingWithDepthData
                     continue;
                 }
                 node = node.Next;
-            }
-        }
-
-    }
-
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        DateTime now;
-        int birdPositionX = 500;
-        int birdPositionY = 50;
-
-        SparkleManager sparkleManager;
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            Canvas.SetLeft(this.birdStatic, 500);
-            Canvas.SetBottom(this.birdStatic, 50);
-
-            this.birdFly.Visibility = Visibility.Hidden;
-
-            System.Diagnostics.Debug.WriteLine("coucou");
-
-            sparkleManager = new SparkleManager(canvas);
-
-            //ImageBehavior.SetAnimatedSource(birdImage, image);
-        }
-        const float MaxDepthDistance = 4095; // max value returned
-        const float MinDepthDistance = 850; // min value returned
-        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
-        
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
-
-        }
-
-        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-
-            var oldSensor = (KinectSensor)e.OldValue;
-
-            //stop the old sensor
-            StopKinect(oldSensor);
-
-            //get the new sensor
-            var newSensor = (KinectSensor)e.NewValue;
-            try
-            {
-                StartKinect(newSensor);
-            }
-            catch (System.IO.IOException)
-            {
-                //this happens if another app is using the Kinect
-                kinectSensorChooser1.AppConflictOccurred();
-            }
-        }
-
-        void newSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
-        {
-            now = DateTime.Now;
-            sparkleManager.Update(now);
-
-            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
-            {
-                if (depthFrame == null)
-                {
-                    return; 
-                }
-
-                byte[] pixels = GenerateColoredBytes(depthFrame);
-
-                //number of bytes per row width * 4 (B,G,R,Empty)
-                int stride = depthFrame.Width * 4;
-
-                //create image
-                image1.Source = 
-                    BitmapSource.Create(depthFrame.Width, depthFrame.Height, 
-                    96, 96, PixelFormats.Bgr32, null, pixels, stride);
-
-                sparkleManager.Generate(now, depthFrame, pixels);
-            }
-        }
-
-        private byte[] GenerateColoredBytes(DepthImageFrame depthFrame)
-        {
-
-            //get the raw data from kinect with the depth for every pixel
-            short[] rawDepthData = new short[depthFrame.PixelDataLength];
-            depthFrame.CopyPixelDataTo(rawDepthData); 
-
-            //use depthFrame to create the image to display on-screen
-            //depthFrame contains color information for all pixels in image
-            //Height x Width x 4 (Red, Green, Blue, empty byte)
-            Byte[] pixels = new byte[depthFrame.Height * depthFrame.Width * 4];
-
-            //Bgr32  - Blue, Green, Red, empty byte
-            //Bgra32 - Blue, Green, Red, transparency 
-            //You must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
-
-            //hardcoded locations to Blue, Green, Red (BGR) index positions       
-            const int BlueIndex = 0;
-            const int GreenIndex = 1;
-            const int RedIndex = 2;
-            int x = 0;
-            int y = 0;
-
-            
-            //loop through all distances
-            //pick a RGB color based on distance
-            for (int depthIndex = 0, colorIndex = 0; 
-                depthIndex < rawDepthData.Length && colorIndex < pixels.Length; 
-                depthIndex++, colorIndex += 4)
-            {
-                x++;
-                if (x == depthFrame.Width)
-                {
-                    y++;
-                    x = 0;
-                }
-                //get the player (requires skeleton tracking enabled for values)
-                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
-
-                //gets the depth value
-                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
-
-                if (depth <= 0)
-                {
-                    //we are very close
-                    pixels[colorIndex + BlueIndex] = 255;
-                    pixels[colorIndex + GreenIndex] = 255;
-                    pixels[colorIndex + RedIndex] = 255;
-
-                }
-                else if (depth > 0 && depth < 2500)
-                {
-                    //we are a bit further away
-                    pixels[colorIndex + BlueIndex] = 0;
-                    pixels[colorIndex + GreenIndex] = 0;
-                    pixels[colorIndex + RedIndex] = 0;
-
-
-                    if (this.birdStatic.Visibility == Visibility.Visible && isNearPoint(x, y, birdPositionX, birdPositionY))
-                    {
-                        this.birdFly.Visibility = Visibility.Visible;
-                        this.birdStatic.Visibility = Visibility.Hidden;
-                        System.Diagnostics.Debug.Write(". On change tout ");
-                    }
-                }
-                else
-                {
-                    //we are the farthest
-                    pixels[colorIndex + BlueIndex] = 255;
-                    pixels[colorIndex + GreenIndex] = 255;
-                    pixels[colorIndex + RedIndex] = 255;
-                }
-
-
-                //////equal coloring for monochromatic histogram
-                //byte intensity = CalculateIntensityFromDepth(depth);
-                //pixels[colorIndex + BlueIndex] = intensity;
-                //pixels[colorIndex + GreenIndex] = intensity;
-                //pixels[colorIndex + RedIndex] = intensity;
-
-
-                ////Color all players "gold"
-                //if (player > 0)
-                //{
-                //    pixels[colorIndex + BlueIndex] = Colors.Black.B;
-                //    pixels[colorIndex + GreenIndex] = Colors.Black.G;
-                //    pixels[colorIndex + RedIndex] = Colors.Black.R;
-                //}
-
-                /*
-                if ((x >= 100 && x <= 200) && (y >= 100 && y <= 150))
-                {
-                    pixels[colorIndex + BlueIndex] = 0;
-                    pixels[colorIndex + GreenIndex] = 0;
-                    pixels[colorIndex + RedIndex] = 255;
-                }*/
-
-
-            }
-          
-
-            return pixels;
-        }
-
-        private bool isNearPoint(int x, int y, int pointPositionX, int pointPositionY)
-        {
-            //System.Diagnostics.Debug.WriteLine("Point detection: ");
-            //System.Diagnostics.Debug.WriteLine(p);
-            return (pointPositionX - 80 <= x && x <= pointPositionX + 80) && (pointPositionY - 80 <= y && y <= pointPositionY + 80);
-        }
-
-
-        public static byte CalculateIntensityFromDepth(int distance)
-        {
-            //formula for calculating monochrome intensity for histogram
-            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0) 
-                / (MaxDepthDistanceOffset)));
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            StopKinect(kinectSensorChooser1.Kinect); 
-        }
-
-        private void StopKinect(KinectSensor sensor)
-        {
-            if (sensor != null)
-            {
-                if (sensor.IsRunning)
-                {
-                    //stop sensor 
-                    sensor.Stop();
-
-                    //stop audio if not null
-                    if (sensor.AudioSource != null)
-                    {
-                        sensor.AudioSource.Stop();
-                    }
-
-
-                }
-            }
-        }
-
-        private void StartKinect(KinectSensor sensor)
-        {
-            if (sensor == null)
-                return;
-
-            //turn on features that you need
-            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-            sensor.SkeletonStream.Enable();
-
-            //sign up for events if you want to get at API directly
-            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(newSensor_AllFramesReady);
-
-            sensor.Start();
-        }
-
-        bool creatingFakeSpakles = false;
-        Point mousePosition;
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                creatingFakeSpakles = true;
-                mousePosition = e.GetPosition(this);
-            }
-        }
-
-        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Released)
-            {
-                creatingFakeSpakles = false;
-                if (e.RightButton == MouseButtonState.Released)
-                {
-                    // create fake sparkle
-                    sparkleManager.AddSparkleFromMouse(mousePosition, e.GetPosition(this), now);
-                }
             }
         }
 
